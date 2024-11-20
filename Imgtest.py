c1d8331288ee72a55c2d50d64a1d29058e275019
snqwq <99054951+snqwq@users.noreply.github.com>
2024-11-19 22:53:17 -0500

    update to fit with asset renaming

--- Imgtest.py
+++ Imgtest.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 picdir = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "assets/album covers"
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "assets/covers"
 )
 libdir = os.path.join(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "lib"
